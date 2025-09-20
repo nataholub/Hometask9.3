e1ee13a577f7fa2509f2d36956cd51519d07e9bf
Natalia Holub <nata.holub@ukr.net>
2025-09-20 17:17:05 +0300

    Hometask9.3

---
+++ Hometask9.3/Program.cs
@@ -0,0 +1,7 @@
+﻿// Ви отримали рядок, який містить декілька слів, розділених комами. Ваша мета - видалити всі пробіли з цього рядка та вивести результат.
+
+string strOriginal = "apple, pear, banana, peach";
+
+string strWithoutSpaces = strOriginal.Replace(" ", "");
+
+Console.WriteLine(strWithoutSpaces);
